b8d5692d24ceafac6ecd0c5ed6d8fe9c0aac2d4d
Sumit saha <smtsh9@gmail.com>
2020-09-07 16:44:37 +0530

    cors

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(cookieparser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     cors({
+        origin: "http://localhost:3000",
         credentials: true,
     })
 );
